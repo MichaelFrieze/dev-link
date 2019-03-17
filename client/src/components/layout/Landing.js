195d3d768ba13a5997e8589a3547f09ec63479e4
Michael Frieze <michaelfrieze@icloud.com>
2019-03-16 20:23:18 -0400

    updated some CSS, text, and layout

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,9 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Link Up With Developers</h1>
+                <h1 className="display-3 mb-4">
+                  Social Network for Developers
+                </h1>
                 <p className="lead">
                   {" "}
                   Create a developer profile/portfolio, share posts and get help
